4820775f4ee21b4ec57770d32f9b7c4e8aaa432d
Johnatan99 <61784936+Johnatan99@users.noreply.github.com>
2024-12-24 14:26:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use(morgan('dev'));
 app.use("/api/v1/auth", require("./routes/userRoutes"))
 app.use("/api/v1/post", require("./routes/postRoutes"))
 
+//Home
+app.get("/", (req,res)=>{
+    res.status(200).send({
+        "success": true,
+        "msg":"Servirdor Node rodando"
+
 //PORT
 const PORT = process.env.PORT || 8085
 
